821aa67df578e80b8e888076f9019e68dea56cdc
klepto <judeosby@gmail.com>
2020-06-25 19:08:53 +0530

    change config var heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: /app/dart-sdk/bin/pub global run aqueduct:aqueduct db upgrade --connect $DATABASE_URL
+release: /app/dart-sdk/bin/pub global run aqueduct:aqueduct db upgrade --connect $HEROKU_POSTGRESQL_COPPER
 web: /app/dart-sdk/bin/pub global run aqueduct:aqueduct serve --port $PORT --config-path heroku.yaml
--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,7 @@ import 'package:size_checker/size_checker.dart';
 Future main() async {
   final app = Application<SizeCheckerChannel>()
       ..options.configurationFilePath = "config.yaml"
-      ..options.port = 80;
+      ..options.port = 8888;
 
   final count = Platform.numberOfProcessors ~/ 2;
   await app.start(numberOfInstances: count > 0 ? count : 1);
