ce4f87e5026056038d672d5523d58c8d013c50a3
klepto <judeosby@gmail.com>
2020-08-02 20:40:35 +0530

    Migration Fix

--- migrations/00000005_unnamed.migration.dart
+++ migrations/00000005_unnamed.migration.dart
@@ -4,7 +4,7 @@ import 'package:aqueduct/aqueduct.dart';
 class Migration5 extends Migration { 
   @override
   Future upgrade() async {
-   		database.addColumn("_Dependency", SchemaColumn("lastAccess", ManagedPropertyType.datetime, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),unencodedInitialValue: '2020-08-02 14:15:23.826274');
+   		database.addColumn("_Dependency", SchemaColumn("lastAccess", ManagedPropertyType.datetime, isPrimaryKey: false, autoincrement: false, isIndexed: false, isNullable: false, isUnique: false),unencodedInitialValue: "'2020-08-02 14:15:23.826274'");
   }
   
   @override
